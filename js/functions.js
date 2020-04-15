96acf1fe0b3737ab43962c6db4b3ba9fe5818e1c
marekokrent <marek.okrent@gmail.com>
2020-04-15 18:55:53 +0200

    add function items

--- js/functions.js
+++ js/functions.js
@@ -8,16 +8,23 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var playerMove;
-playerMove = 'kamien';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
+}
+else {
+    computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove;
+playerMove = 'kamien';
+
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to: ' + playerMove + ', to wygrywasz!');
