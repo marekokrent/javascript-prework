034c994acdfa15846d948b8a2dcf80843ec3225d
marekokrent <marek.okrent@gmail.com>
2020-04-15 22:13:14 +0200

    add function nameAssignment

--- js/functions.js
+++ js/functions.js
@@ -8,23 +8,42 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+function nameAssignment(Number){
+    if (Number == '1') {
+    return 'kamień';
+    } else if (Number == '2') {
+    return 'papier';
+    } else if (Number == '3') {
+    return 'nożyce';
+    }
+}
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else if (randomNumber == 3) {
-  computerMove = 'nożyce';
+console.log('wylosowana cyfra to: ' + randomNumber);
+
+computerMove = nameAssignment(randomNumber);
+console.log('przypisano zmienna:' + computerMove);
+
+
+
+var playerMove, playerInput;
+
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('wpisana cyfra to: ' + playerInput);
+
+
+
+if (playerInput <= '3' && playerInput >= '1') {
+    playerMove = nameAssignment(playerInput);
+  } else {
+    playerMove = 'nieznany ruch';
+  }
+
+console.log('przypisano zmienna:' + playerMove);
+
+if (playerMove != 'nieznany ruch') {
+
+  printMessage('Zagrałem ' + computerMove + ' Twój ruch to: ' + playerMove + ' Wynik');
+
 }
-else {
-    computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-
-var playerMove;
-playerMove = 'kamien';
-
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to: ' + playerMove + ', to wygrywasz!');
