2cf84b4c7d19c4d16f28e7814d592b5ba5a8fcbd
marekokrent <marek.okrent@gmail.com>
2020-04-15 15:13:38 +0200

    add combination of the random function and the conditional function

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,12 @@ var playerMove;
 playerMove = 'kamien';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-var randomNumber;
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
